4fc46a736aff2bce48b1277ad14421e7ad791816
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2022-10-13 10:34:24 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+import streamlit as st
+deupau = False
+i = 0
+calculodelta = st.number_input(label="etiqueta")
+while deupau == False:
+  i += 1
+  divisao = calculodelta / i
+  resultado = divisao * i
+  if divisao == i: 
+    i2 = divisao 
+    deupau = True 
+  else: 
+    deupau = False
+st.write(i2)
