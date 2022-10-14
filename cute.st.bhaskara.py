ce0a2071194185153c9a03ffa746bb80b6864e1a
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2022-10-13 21:00:36 -0300

    Update cute.st.bhaskara.py

--- cute.st.bhaskara.py
+++ cute.st.bhaskara.py
@@ -21,7 +21,7 @@ st.write("----------------------------")
 st.write("2A")
 st.write("")
 #Imagem01
-st.image("https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo/formula-de-bhaskara.jpg")
+st.image("formula-de-bhaskara.jpg")
 st.write ("Calcula a Bhaska ai pra Noiz!")
 #USER st.number_input
 st.write("Qual Ã© o valor de A?")
@@ -40,7 +40,7 @@ st.write("")
 st.write("Valor de C ", c)
 #calculodelta
 #Imagem 02
-st.image("http://2.bp.blogspot.com/-v--lekxUbt0/VXr_4sJPvCI/AAAAAAAAQrU/43sKNsaLX58/s320/delta.jpg")
+st.image("delta.jpg")
 st.write("")
 st.write("Calculo do Delta")
 calculodelta = (b * b) - (4 * a * c)
@@ -55,7 +55,7 @@ st.write("Delta = ", calculodelta)
 
 #(calculo raiz)
 st.write("")
-st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEihbOPtXQwp9mlv8Ez59puk8kkIY70PLQj4LatwhngTjs98omoFFutLy7NQHYg3UDUiJ_PmtDgExofpzK3vy4X9iIPXCI_rw-lzNauwKjLTWkwHCZ19oVCbuUa-9Qs_DQl5p5I-da1x8zqLYF14ZGemrWf-bcj2aMYl1I9Xa8wxgjwfvJ97K202zHZ3/w433-h244/nomenclatura%20raiz%20quadrada.png")
+st.image("raiz-quadrada.png")
 st.write("")
 st.write("Calculo da Raiz")
 #While resultado for diferente de deltaraiz
@@ -81,7 +81,7 @@ st.write("Valor de I atualizado: ",i)
 st.write("Raiz de Delta: ",i2)
 st.write("")
 #imagem 04
-st.image("https://www.pravaler.com.br/wp-content/uploads/2020/08/formula-de-bhaskara-calcular-raizes.png")
+st.image("x1_x2.png")
 st.write("")
 st.write("Calculo X1 e X2")
 st.write("X1")
@@ -116,7 +116,7 @@ st.write("")
 #de resultado na impressao do console
 
 st.write("Olha o veio Baka ae, quer dizer, Bhaskara")
-st.image("https://matematica.com.br/image/posts/1490471516.png")
+st.image("bhaskara.png")
 st.write("")
 st.write("MAS....")
 st.video("https://www.youtube.com/watch?v=39OSPLVI3_I")
