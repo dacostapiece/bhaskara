9ebb0177801cc2fdbb528583da422cb1b1b9df1f
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2022-10-14 12:32:28 -0300

    Update button.py

--- button.py
+++ button.py
@@ -1,1 +1,10 @@
-
+import streamlit as st
+name=""
+age=0
+buttonName = st.button("insert name")
+if buttonName == True:
+  name = st.text_input("nome")
+  buttonName = True
+age = st.number_input("age", step=1, value=0)
+st.write("name: ", name)
+st.write("age", age)
