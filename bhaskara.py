393bbd8a57015f6352a3236e98fac5fbf6c45e84
dacostapiece <101139745+dacostapiece@users.noreply.github.com>
2023-02-03 17:32:17 -0300

    Update bhaskara.py
    Agora rodando

--- bhaskara.py
+++ bhaskara.py
@@ -1,128 +1,159 @@
+#! /usr/bin/python3
 import streamlit as st
 st.title ("Formula de Bhaskara")
+#upper header
 def run():
- st.set_page_config(
- page_title="Formula de Bhaskara",
- page_icon="",
+  st.set_page_config(
+  page_title="Formula de Bhaskara",
+  page_icon="",
 )
 
-st.write("")
-i = 0
-deupau = False
-st.write("Formula de Bhaskara") 
-st.write("")
-st.write("X1=-B+Raiz(Delta)")
-st.write("--------------------")
-st.write("2A")
-st.write("")
-st.write("X2=-B-Raiz(Delta)")
-st.write("----------------------------")
-st.write("2A")
 st.write("")
 #Imagem01
 st.image("formula-de-bhaskara.jpg")
 st.write ("Calcula a Bhaska ai pra Noiz!")
 #USER st.number_input
-st.write("Qual Ã© o valor de A?")
-a = st.number_input("SugestÃ£o 1: ", min_value=None, max_value=None, value=1, step=1)
+st.write("Qual eh o valor de A?")
+a = st.number_input("Sugestao 1: ", min_value=None, max_value=None, value=1, step=1, key="a")
 st.write("")
 st.write("Valor de A: ", a)
 
-st.write("Qual Ã© o valor de B? ")
-b = st.number_input("SugestÃ£o 8: ", min_value=None, max_value=None, value=8, step=1)
+st.write("Qual eh o valor de B? ")
+b = st.number_input("Sugestao 8: ", min_value=None, max_value=None, value=8, step=1, key="b")
 st.write("")
 st.write("Valor de B ", b)
 
-st.write("Qual Ã© o valor de C?")
-c = st.number_input("SugestÃ£o -9: ", min_value=None, max_value=None, value=-9, step=1)
+st.write("Qual eh o valor de C?")
+c = st.number_input("Sugestao -9: ", min_value=None, max_value=None, value=-9, step=1, key="c")
 st.write("")
 st.write("Valor de C ", c)
 #calculodelta
+
+#FUNCAO calculoDelta
+def calculoDelta(a,b,c):
+  resultadoDelta = (b * b) - (4 * a * c)
+  return resultadoDelta
+
+#st.write("PONTO CONTROLE - Print calculoDelta", calculoDelta(a,b,c))
+
+resultadoDelta = calculoDelta(a,b,c)
+
+#st.write("Delta01 = ", resultadoDelta)
+
 #Imagem 02
 st.image("delta.jpg")
 st.write("")
 st.write("Calculo do Delta")
-calculodelta = (b * b) - (4 * a * c)
+
 st.write("Valores previos")
 st.write("A = ", a)
 st.write("B = ", b)
 st.write("C = ", c)
 st.write("")
-st.write("Delta = B2 - 4AC")
+st.latex(r'''\Delta=b^2-4ac''')
 st.write("Delta = ","(",b,"*",b,") - (4*",a,"*",c,")")
-st.write("Delta = ", calculodelta)
+st.write("Delta = ", resultadoDelta)
 
 #(calculo raiz)
-st.write("")
+st.write("Exemplo de Cálculo da Raiz Quadrada")
 st.image("raiz-quadrada.png")
 st.write("")
-st.write("Calculo da Raiz")
-#While resultado for diferente de deltaraiz
-st.write("")
-st.write("Inicio do loop Calculo RAIZ")
+st.write("Calculando Raiz")
 
-st.write("Valor Delta:", calculodelta)
-st.write("Valor I inicial:", i)
-while deupau == False:
- i += 1
- 
- divisao = calculodelta / i
- 
- resultado = divisao * i
- if divisao == i: 
-  i2 = divisao 
-  deupau = True 
- else: 
+st.write("")
+
+#FUNCAO calculoRaiz
+def calculoRaiz(resultadoDelta):
   deupau = False
+  str_depau = "False"
+  i = 0
+  percent_complete=0
+  while deupau == False:
+    i += 1
+    st.write("<span style='color:red'>CALCULO RAIZ INTERAÇÃO:</span>",i,unsafe_allow_html=True)
+    st.progress(percent_complete+i)
+   
+    divisao = resultadoDelta / i
+    
+    resultadoRaiz = divisao
+    
+    if divisao == i: 
+      deupau = True
+      str_depau = "True"
+      
+    else:
+      bogus = 0
+
+  return resultadoRaiz
+st.write("")
+#calculoRaiz(resultadoDelta)
+resultadoRaiz = calculoRaiz(resultadoDelta)
 
 st.write("")
-st.write("Valor de I atualizado: ",i) 
-st.write("Raiz de Delta: ",i2)
+st.write("Raiz de Delta"); st.latex(r'''\sqrt{\Delta}''')
+st.write("Raiz de Delta:",resultadoDelta,": ",resultadoRaiz)
 st.write("")
 #imagem 04
+st.write("Exemplo de Cálculo X1 e X2")
 st.image("x1_x2.png")
 st.write("")
 st.write("Calculo X1 e X2")
-st.write("X1")
-st.write("X1 = - B + Raiz(Delta)")
-st.write("----------------------------")
-st.write("2 * A")
-st.write("")
-st.write("X1 = -",b,' + ',i2)
-st.write("----------------------------")
-st.write("2 * ", a)
-st.write("")
-x1=-(b+i2)/2*a
-x2=-(b-i2)/2*a
-st.write("X1 = ", x2)#
-st.write("")
-st.write("X2")
-st.write("X2 = - B - Raiz(Delta)")
-st.write("----------------------------")
-st.write("2 * A")
-st.write("")
-st.write("X2 = -",b,' - ',i2)
-st.write("----------------------------")
-st.write("2 * ", a)
-st.write("")
+st.latex(r'''\Chi=\frac {-b^2±\sqrt{\Delta}} {2.a}''')
 
-st.write("X2 = ", x1)
-st.write("")
+#FUNCAO calculoX1_X2
+def calculoX1_X2(b,calculoRaiz,a):
+    #X1
+    st.write("Calculo X1")
+    st.latex(r'''\Chi'=\frac {-b^2+\sqrt{\Delta}} {2.a}''')
+    divisao = 2*a
+       
+    st.write("")
+    
+    st.write("x=-(+",b,")+-RAIZ(",resultadoRaiz*resultadoRaiz,")/2a")
+    st.write("x1 = ",-b,"+",resultadoRaiz,"/",divisao)
+    passo02=-b+resultadoRaiz
+    passo03=passo02/divisao
+    X1 = passo03
+    st.write("X1 = ",passo02,"/",divisao)
+    st.write("X1 = ",passo03)
+    
+    #X2
+    st.write("")
+    st.write("Calculo X2")
+    st.latex(r'''\Chi''=\frac {-b^2-\sqrt{\Delta}} {2.a}''')
 
-#por algum problema de sintaxe python ou logica matematica 
-#o calculo esta invertendo sinal positivo com negativo
-#entao para nao perder mais tempo, inverti as variaveis
-#de resultado na impressao do console
+    st.write("x=-(+",b,")+-RAIZ(",resultadoRaiz*resultadoRaiz,")/2a")
+    st.write("X2= ",-b,"-",resultadoRaiz,"/",divisao)
+    passo02_x2=-b-resultadoRaiz
+    passo03_x2=passo02_x2/divisao
+    st.write("X2 = ",passo02_x2,"/",divisao)
+    st.write("X2 = ",passo03_x2)
+    X2 = passo03_x2
+    arrayX1_X2 = [X1,X2]
+
+    return arrayX1_X2
+resultadoX1 = calculoX1_X2(b,resultadoRaiz,a)
+
+X1 = resultadoX1[0]
+X2 = resultadoX1[1]
+
+st.write("")
 
 st.write("Olha o veio Baka ae, quer dizer, Bhaskara")
-#st.image("bhaskara.png")
+st.image("bhaskara.png")
 st.write("")
 st.write("MAS....")
 st.video("https://www.youtube.com/watch?v=39OSPLVI3_I")
 
-st.write("Site de referÃªncia")
+st.write("Site de referencia")
 st.write("https://mundoeducacao.uol.com.br/matematica/formula-bhaskara.htm")
 st.write("Full code snippet")
 st.write("https://github.com/dacostapiece/dc/blob/main/code-bhaskara.py")
 st.write("Obrigado!! Mafia Dac")
 st.write("")
+
+#Inserir disclaimer de quando de fato rodou o código
+#st.video("https://www.youtube.com/watch?v=39OSPLVI3_I")
+
+st.write("P.S.:")
+st.write("Esse código não trata se Delta for negativo ou se Raiz for igual à Zero")
